0bf1040ea3f3a03d18ab13fd8ed959c6d5e1a9fc
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-25 09:49:24 +0700

    *REACT TRAVEL LIST - Calculating statistik and Derived state

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItems}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -51,8 +51,6 @@ function Form({ onAddItems }) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-
     onAddItems(newItem);
 
     setDescription("");
@@ -116,10 +114,24 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numberPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numberPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go✈️"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numberPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
